3fe4f9f4a1a390870c308b3627ed2f48179fbc74
Jamal <warijamalk@gmail.com>
2025-10-02 14:26:40 +0300

    Adding MongoDB Setup

--- app.js
+++ app.js
@@ -2,15 +2,9 @@ const path = require("path");
 
 const express = require("express");
 const bodyParser = require("body-parser");
-const sequelize = require("./util/db");
-
-// Import our Models here:
-const Product = require("./models/product");
-const User = require("./models/user");
-const Cart = require("./models/Cart");
-const CartItem = require("./models/cart-Item");
 
 const errorController = require("./controllers/error");
+const mongo = require("./util/db");
 
 const app = express();
 
@@ -27,69 +21,29 @@ const app = express();
 app.set("view engine", "ejs");
 app.set("views", "views");
 
-const adminRoutes = require("./routes/admin");
-const shopRoutes = require("./routes/shop");
+// const adminRoutes = require("./routes/admin");
+// const shopRoutes = require("./routes/shop");
 
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(express.static(path.join(__dirname, "public")));
 
 app.use((req, res, next) => {
-  User.findByPk(1)
-    .then((user) => {
-      req.user = user;
-      next();
-    })
-    .catch((error) => {
-      console.log(error);
-    });
+  // User.findByPk(1)
+  //   .then((user) => {
+  //     req.user = user;
+  //     next();
+  //   })
+  //   .catch((error) => {
+  //     console.log(error);
+  //   });
 });
 
-app.use("/admin", adminRoutes);
-app.use(shopRoutes);
+// app.use("/admin", adminRoutes);
+// app.use(shopRoutes);
 
 app.use(errorController.get404);
 
-//  Sync Method Helps by creating our table in our database where it has
-// a feature to look our models file and create whatever tables we have
-// their but if and only if it is  not created before so it doesn't
-// override our table
-
-// Here we will declare our relations before synchronizing it to database:
-// onDelete property insures if the User has been deleted any  products done
-// by the user will be deleted as well;
-Product.belongsTo(User, { constraints: true, onDelete: "CASCADE" });
-User.hasMany(Product);
-User.hasOne(Cart);
-Cart.belongsTo(User);
-// Since Many to Many relations require third table to store productID key
-// and CartID key so here comes the through property it tells mysql where
-// to store those keys in otherwords our third table is "CartItem";
-Cart.belongsToMany(Product, { through: CartItem });
-Product.belongsToMany(Cart, { through: CartItem });
-
-sequelize
-  // Now sync's job is to create those models and add them to DB but if we want
-  // to alter the database we can use propert called "force" where it will
-  // force the changes we added to the database by dropping the old tables
-  // and replace them with the new ones including the relations(Associations).
-  // .sync({ force: true })
-  .sync()
-  .then((result) => {
-    // console.log(result);
-    return User.findByPk(1);
-  })
-  .then((user) => {
-    if (!user) {
-      return User.create({ name: "Khaled", email: "k@test.com" });
-    }
-    return user;
-  })
-  .then((result) => {
-    return result.createCart();
-  })
-  .then((cart) => {
-    app.listen(3000);
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+mongo((client) => {
+  app.listen(3000);
+  console.log(client);
+});
--- util/db.js
+++ util/db.js
@@ -1,19 +1,19 @@
-// You can think of Sequelize as a class thats why we need to write in the
-// capital letter "S" when we import it.
-const Sequelize = require("sequelize");
-//  Here we want to establish the connection using Sequelize package
-//  We need to create new instance of Sequelize just like we did below also
-//  we need to be aware of the parameters we pass it has to at this order
-//  to avoid complications ("Database Name", "Username", "mysql Password",{
-//  The "dialect" is just telling what database server you want to
-//  use here we said mysql.
-//  "host" is optional if you want to write here because in default it
-//  will use localhost unless you set it otherwise.
-// })
-const conn = new Sequelize("node-complete", "root", "12345", {
-  dialect: "mysql",
-  host: "localhost",
-});
-// Finally we export like we always do so we can use in different
-// files in our project.
-module.exports = conn;
+const mongodb = require("mongodb");
+
+const MongoClient = mongodb.MongoClient;
+const mongoConnect = (callback) => {
+  MongoClient.connect(
+    "mongodb+srv://JKW:z4dFjSmIzzwK6RVD@cluster0.jzrmoxo.mongodb.net/?retryWrites=true&w=majority&appName=Cluster0"
+  )
+    .then((client) => {
+      console.log("Connected!!!");
+      callback(client);
+    })
+    .catch((error) => {
+      console.log(error);
+
+      console.log("Not Connected");
+    });
+};
+// We are exporting a function here because mongoConnect is a function
+module.exports = mongoConnect;
